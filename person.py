6027b532022b8d0866bea49c7b6f252f9abeb451
edgar <edgarv@10web.io>
2021-05-30 21:04:00 +0400

    Added code for simulating 3 policies

--- person.py
+++ person.py
@@ -1,19 +1,42 @@
 from __future__ import annotations
 
+import random
+from enum import Enum
 import numpy as np
 from typing import Optional
 
 
+class PolicyEnum(Enum):
+    none = 'none'
+    antigen = 'antigen'
+    antigen_antibody = 'antigen_antibody'
+
+
 class Person:
-    def __init__(self):
+    def __init__(self,
+                 strategy: PolicyEnum = PolicyEnum.none,
+                 infection_probability: float = 0.1,
+                 immunity_probability: float = 0.25):
         self.contagiousness = 0
         self.contagious_days: Optional[int] = None
         self.symptoms = False
         self.immunity = False
         self.immune_days: Optional[int] = None
+        self.immunity_length = 180
+        self.quarantined = False
+        self.days_quarantined = None
+        self.quarantine_length = 14
+        self.knows_antibody = False
+        self.knows_antibody_days = None
+        self.strategy = strategy
+        if random.uniform(0, 1) < infection_probability:
+            self.infect()
+        if random.uniform(0, 1) < immunity_probability:
+            self.immunity = True
+            self.immune_days = 0
 
     def infect(self):
-        self.contagiousness = np.random.beta(a=6, b=6)
+        self.contagiousness = np.random.beta(a=5, b=7)
         self.contagious_days = 0
         self.symptoms = False
 
@@ -24,14 +47,35 @@ class Person:
         self.immunity = True
         self.immune_days = 0
 
+    def is_infected(self):
+        return self.contagiousness > 0
+
     def remove_immunity(self):
         self.immunity = False
         self.immune_days = None
 
+    def test_antigen(self):
+        if self.is_infected():
+            self.quarantined = True
+            self.days_quarantined = 0
+            self.knows_antibody = True
+            self.knows_antibody_days = 0
+
+    def test_antibody(self):
+        if self.contagious_days is not None and self.contagious_days > 8:
+            self.quarantined = True
+            self.days_quarantined = 0
+            self.knows_antibody = True
+            self.knows_antibody_days = 0
+        elif self.immunity:
+            self.knows_antibody = True
+            self.knows_antibody_days = 0
+
     def meet_with_other(self, other: Person):
         if not self.contagiousness and \
-                other.contagiousness and np.random.uniform() < other.contagiousness:
+                other.contagiousness and np.random.uniform() < other.contagiousness and not other.symptoms and not self.immunity:
             self.infect()
+            return True
 
     def pass_day(self):
         if self.contagious_days is not None:
@@ -40,5 +84,20 @@ class Person:
                 self.symptoms = True
             elif self.contagious_days == 14:
                 self.heal()
-        if self.immunity and self.immune_days == 180:
+        if self.days_quarantined is not None:
+            self.days_quarantined += 1
+            if self.days_quarantined == self.quarantine_length:
+                self.quarantined = False
+                self.quarantine_length = None
+        if self.knows_antibody_days is not None:
+            self.knows_antibody_days += 1
+            if self.knows_antibody_days == 170:
+                self.knows_antibody = False
+                self.knows_antibody_days = None
+        if self.immunity and self.immune_days == self.immunity_length:
             self.remove_immunity()
+        elif self.immunity:
+            self.immune_days += 1
+        if not self.immunity and random.uniform(0, 1) < 1 / 10000 and not self.is_infected():
+            self.infect()
+            return True
--- process.py
+++ process.py
@@ -1,15 +1,115 @@
-import numpy as np
+from random import choice
+from person import Person, PolicyEnum
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+import pandas as pd
 
-from person import Person
 
 class Processor:
-    def __init__(self, population: int = 1000, days: int = 365):
+    def __init__(self,
+                 population: int = 1000,
+                 initial_infection_ratio: float = 0.001,
+                 initial_immunity_ratio: float = 0.25,
+                 days: int = 365,
+                 strategy: PolicyEnum = PolicyEnum.none):
+        self.budget = 0
+        self.infection_count = 0
         self.population = population
-        self.people = [Person() for _ in range(population)]
-        self.people[0].infect()  # patient zero
+        self.people = [Person(strategy=strategy,
+                              infection_probability=initial_infection_ratio,
+                              immunity_probability=initial_immunity_ratio)
+                       for _ in range(population)]
         self.days = days
+        self.strategy = strategy
 
     def process(self):
-        for day in range(self.days):
-            meetings = 2 * self.population
-            pairs = [np.random.choice(self.people, 2, replace=False)]
+        counts = []
+        for day in tqdm(range(self.days)):
+            count = len([x for x in self.people if x.is_infected()])
+            counts.append(count)
+            meetings = int(0.5 * self.population)
+            pairs = [(choice(self.people), choice(self.people)) for _ in range(meetings)]
+            for person1, person2 in pairs:
+                if person1 == person2:
+                    continue
+                if person1.quarantined or person2.quarantined:
+                    continue
+                p1infected = bool(person1.meet_with_other(person2))
+                p2infected = bool(person2.meet_with_other(person1))
+                self.infection_count += p1infected
+                self.infection_count += p2infected
+            if self.strategy == PolicyEnum.antigen:
+                people_to_test = self.people[day % 14::14]
+                [person.test_antigen() for person in people_to_test]
+                self.budget += len(people_to_test) * 10
+            elif self.strategy == PolicyEnum.antigen_antibody:
+                people_with_no_antibody = [person for person in self.people if not person.knows_antibody]
+                people_to_test_antigen = people_with_no_antibody[day % 14::14]
+                people_to_test_antigen.extend([x for x in self.people if x.symptoms and not x.quarantined])
+                [person.test_antigen() for person in people_to_test_antigen]
+                self.budget += len(people_to_test_antigen) * 10
+                people_with_no_antibody = [person for person in self.people if not person.knows_antibody]
+                people_to_test_antibody = people_with_no_antibody[day % 60::60]
+                [person.test_antigen() for person in people_to_test_antibody]
+                self.budget += len(people_to_test_antibody) * 10
+
+            for person in self.people:
+                infected = bool(person.pass_day())
+                self.infection_count += infected
+        return counts, self.budget, self.infection_count
+
+
+if __name__ == '__main__':
+    population_range = [1000, 10000, 100000, 3000000]
+    none_budgets = []
+    antigen_budgets = []
+    antigen_antibody_budgets = []
+    none_counts = []
+    antigen_counts = []
+    antigen_antibody_counts = []
+    for population_ in population_range:
+        initial_infection_ratio_ = 0.001
+        initial_immunity_ratio_ = 0.25
+        processor = Processor(population=population_,
+                              initial_infection_ratio=initial_infection_ratio_,
+                              initial_immunity_ratio=initial_immunity_ratio_,
+                              strategy=PolicyEnum.none)
+        counts_none, budget_none, infection_count_none = processor.process()
+        none_budgets.append(budget_none)
+        none_counts.append(infection_count_none)
+        plt.plot(counts_none, label="none")
+        processor = Processor(population=population_,
+                              initial_infection_ratio=initial_infection_ratio_,
+                              initial_immunity_ratio=initial_immunity_ratio_,
+                              strategy=PolicyEnum.antigen)
+        counts_antigen, budget_antigen, infection_count_antigen = processor.process()
+        antigen_budgets.append(budget_antigen)
+        antigen_counts.append(infection_count_antigen)
+        plt.plot(counts_antigen, label="antigen")
+        processor = Processor(population=population_,
+                              initial_infection_ratio=initial_infection_ratio_,
+                              initial_immunity_ratio=initial_immunity_ratio_,
+                              strategy=PolicyEnum.antigen_antibody)
+        counts_antigen_antibody, budget_antigen_antibody, infection_count_antigen_antibody =\
+            processor.process()
+        antigen_antibody_budgets.append(budget_antigen_antibody)
+        antigen_antibody_counts.append(infection_count_antigen_antibody)
+        plt.plot(counts_antigen_antibody, label="antigen_antibody")
+        plt.legend()
+        plt.savefig(f"{population_}_people.png")
+        plt.clf()
+    df = pd.DataFrame(list(zip(none_budgets,
+                               antigen_budgets,
+                               antigen_antibody_budgets,
+                               none_counts,
+                               antigen_counts,
+                               antigen_antibody_counts)), columns=[
+        "none_budgets",
+        "antigen_budgets",
+        "antigen_antibody_budgets",
+        "none_counts",
+        "antigen_counts",
+        "antigen_antibody_counts"])
+    df.index = population_range
+
+    df.to_csv('results.csv')
